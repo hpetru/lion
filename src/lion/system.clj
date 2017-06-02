5f87b9c802a69bc091fdfc00dd4f28982f0ef0b7
Petru Hincu <hincupetru@gmail.com>
2017-06-02 19:44:58 +0300

    new changes

--- src/lion/system.clj
+++ src/lion/system.clj
@@ -2,7 +2,10 @@
   (:require [com.stuartsierra.component :as component]
             [clojure.core.async :as async]
             [lion.worker :refer [map->Worker]]
-            [lion.queue :refer [map->Queue]]))
+            [lion.queue :refer [map->Queue]]
+            [lion.facebook :refer [map->Facebook facebook-puller]]
+            [lion.twitter :refer [map->Twitter twitter-puller twitter-streamer]]
+            ))
 
 (defn system
   [config]
@@ -35,26 +38,29 @@
                               {:incoming-chan :twitter-streamer-output-chan
                                :outgoing-chan :twitter-streamer-input-chan})
 
-    ; TODO: Write an real :work-fn
+    :twitter-client (map->Twitter (:twitter-client config))
+    :facebook-client (map->Facebook (:facebook-client config))
+
     ; Facebook puller
     :worker-facebook-puller (component/using
                               (map->Worker
-                                {:work-fn (fn [_ x] (println x) x)})
+                                {:work-fn facebook-puller}
                               {:input-chan :facebook-puller-input-chan
-                                :output-chan :facebook-puller-output-chan})
+                                :output-chan :facebook-puller-output-chan
+                                :client :facebook-client})
 
-    ; TODO: Write an real :work-fn
     ; Twitter puller
     :worker-twitter-puller (component/using
                              (map->Worker
-                                {:work-fn (fn [_ x] (println x) x)})
+                                {:work-fn twitter-puller}
                              {:input-chan :twitter-puller-input-chan
-                              :output-chan :twitter-puller-output-chan})
+                              :output-chan :twitter-puller-output-chan
+                              :client :twitter-client})
 
-    ; TODO: Write an real :work-fn
     ; Twitter streamer
     :worker-twitter-streamer (component/using
                                (map->Worker
-                                 {:work-fn (fn [_ x] (println x) x)})
+                                 {:work-fn twitter-streamer}
                                {:input-chan :twitter-streamer-input-chan
-                                :output-chan :twitter-streamer-output-chan})))
+                                :output-chan :twitter-streamer-output-chan
+                                :client :twitter-client})))
