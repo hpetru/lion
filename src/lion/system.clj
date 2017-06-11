e942d6153c4b243cfd2b2685285ad706c0e854fd
Petru Hincu <hincupetru@gmail.com>
2017-06-11 21:52:12 +0300

    some new stuff

--- src/lion/system.clj
+++ src/lion/system.clj
@@ -7,94 +7,122 @@
             ; [lion.twitter :refer [map->Twitter twitter-puller twitter-streamer]]
             ))
 
+; Dummy functions
+(defn facebook-puller
+  [component value]
+  )
+
+(defn twitter-puller
+  [component value]
+  )
+
+(defn twitter-streamer
+  [component value]
+  (let [restart-chan (:restart-chan component)]
+    ; Check if you receive any messages here from restart chan
+    ; and stop streamer
+    )
+  )
+
 (defn system
   [config]
-  {})
+  (component/system-map
+    ; Channels for facebook puller
+    ; ------------------------------
+    :facebook-puller-input-chan
+    (async/chan 8)
 
-;(defn system
-;  [config]
-;  (component/system-map
-;    ; Channels for facebook puller
-;    ; ------------------------------
-;    :facebook-puller-input-chan
-;    (async/chan 8)
-;
-;    :facebook-puller-output-chan
-;    (async/chan 8)
-;    ; ------------------------------
-;
-;    ; Channels for twitter puller
-;    ; ------------------------------
-;    :twitter-puller-input-chan
-;    (async/chan 8)
-;
-;    :twitter-puller-output-chan
-;    (async/chan 8)
-;    ; ------------------------------
-;
-;    ; Channels for twitter streamer
-;    ; ------------------------------
-;    :twitter-streamer-input-chan
-;    (async/chan 8)
-;
-;    :twitter-streamer-output-chan
-;    (async/chan 8)
-;    ; ------------------------------
-;
-;    ; ------------------------------
-;    :facebook-puller-queue
-;    (component/using
-;      (map->Queue (:twitter-puller-queue config))
-;      {:incoming-chan :facebook-puller-output-chan
-;       :outgoing-chan :facebook-puller-input-chan})
-;
-;
-;    ; ------------------------------
-;    :twitter-puller-queue
-;    (component/using
-;      (map->Queue (:twitter-puller-queue config))
-;      {:incoming-chan :twitter-puller-output-chan
-;       :outgoing-chan :twitter-streamer-input-chan})
-;
-;    ; ------------------------------
-;    :twitter-streamer-queue
-;    (component/using
-;      (map->Queue (:twitter-puller-queue config))
-;      {:incoming-chan :twitter-streamer-output-chan
-;       :outgoing-chan :twitter-streamer-input-chan})
-;
-;    ; ------------------------------
-;    :twitter-client
-;    (map->Twitter (:twitter-client config))
-;
-;    ; ------------------------------
-;    :facebook-client
-;    (map->Facebook (:facebook-client config))
-;
-;    ; Facebook puller
-;    ; ------------------------------
-;    :worker-facebook-puller
-;    (component/using
-;      (map->Worker {:work-fn facebook-puller})
-;      {:input-chan :facebook-puller-input-chan
-;       :output-chan :facebook-puller-output-chan
-;       :client :facebook-client})
-;
-;    ; Twitter puller
-;    ; ------------------------------
-;    :worker-twitter-puller
-;    (component/using
-;      (map->Worker {:work-fn twitter-puller})
-;      {:input-chan :twitter-puller-input-chan
-;       :output-chan :twitter-puller-output-chan
-;       :client :twitter-client})
-;
-;    ; Twitter streamer
-;    ; ------------------------------
-;    :worker-twitter-streamer
-;    (component/using
-;      (map->Worker {:work-fn twitter-streamer})
-;      {:input-chan :twitter-streamer-input-chan
-;       :output-chan :twitter-streamer-output-chan
-;       :client :twitter-client})
-;    ))
+    :facebook-puller-output-chan
+    (async/chan 8)
+    ; ------------------------------
+
+    ; Channels for twitter puller
+    ; ------------------------------
+    :twitter-puller-input-chan
+    (async/chan 8)
+
+    :twitter-puller-output-chan
+    (async/chan 8)
+    ; ------------------------------
+
+    ; Channels for twitter streamer
+    ; ------------------------------
+    :twitter-streamer-input-chan
+    (async/chan 1)
+
+    :twitter-streamer-output-chan
+    (async/chan 8)
+
+    ; This chan was added to allow us to
+    ; restart twitter streamer
+    ; ------------------------------
+    :twitter-streamer-restart-chan
+    (async/chan)
+
+    ; ------------------------------
+    :facebook-puller-queue
+    (component/using
+      (map->Queue (:twitter-puller-queue config))
+      {:incoming-chan :facebook-puller-output-chan
+       :outgoing-chan :facebook-puller-input-chan})
+
+
+    ; ------------------------------
+    :twitter-puller-queue
+    (component/using
+      (map->Queue (:twitter-puller-queue config))
+      {:incoming-chan :twitter-puller-output-chan
+       :outgoing-chan :twitter-streamer-input-chan})
+
+    ; ------------------------------
+    :twitter-streamer-queue
+    (component/using
+      (map->Queue (:twitter-streamer-queue config))
+      {:incoming-chan :twitter-streamer-output-chan
+       :outgoing-chan :twitter-streamer-input-chan})
+
+    ;------------------------------
+    :twitter-streamer-restart-queue
+    (component/using
+      (map->Queue (:twitter-streamer-restart-queue config))
+      {:incoming-chan :twitter-streamer-restart-chan
+       :outgoing-chan (async/chan)})
+
+    ; ------------------------------
+    :twitter-client
+    (assoc :twitter-client true)
+    ;(map->Twitter (:twitter-client config))
+
+    ; ------------------------------
+    :facebook-client
+    (assoc :facebook-client true)
+    ;(map->Facebook (:facebook-client config))
+
+    ; Facebook puller
+    ; ------------------------------
+    :worker-facebook-puller
+    (component/using
+      (map->Worker {:work-fn facebook-puller})
+      {:input-chan :facebook-puller-input-chan
+       :output-chan :facebook-puller-output-chan
+       :client :facebook-client})
+
+    ; Twitter puller
+    ; ------------------------------
+    :worker-twitter-puller
+    (component/using
+      (map->Worker {:work-fn twitter-puller})
+      {:input-chan :twitter-puller-input-chan
+       :output-chan :twitter-puller-output-chan
+       :client :twitter-client})
+
+    ; Twitter streamer
+    ; ------------------------------
+    :worker-twitter-streamer
+    (component/using
+      (map->Worker {:work-fn twitter-streamer})
+      {:input-chan :twitter-streamer-input-chan
+       :output-chan :twitter-streamer-output-chan
+       :restart-chan :twitter-streamer-restart-chan
+       :client :twitter-client})
+    ))
